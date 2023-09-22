29620e48e7d2647afa0fdf6dd63152afd6496958
Felix <felixbruguera0@gmail.com>
2023-09-22 08:11:44 -0400

    added the change size button and function

--- script.js
+++ script.js
@@ -1,29 +1,47 @@
-
-const container = document.querySelector("#container");
-for (let i = 0; i < 16;i++) {
-    let div = document.createElement("div");
-    div.classList.add("divs");
-    div.dataset.mainDiv = i;
-    container.appendChild(div);
-}
-const divs = document.querySelectorAll(".divs");
-for (const parent of divs) {
-    for (let i = 0; i < 16;i++) {
-    let smallDiv = document.createElement("div");
-    smallDiv.classList.add("smallDiv");
-    smallDiv.dataset.smallDiv = i;
-    parent.appendChild(smallDiv);
-    }
-};
+let grid = 16;
 const body = document.querySelector("body");
+const container = document.querySelector("#container");
+const gridButton = document.querySelector("#gridButton");
+
+function makeSmallDivs () {
+        for (let i = 0; i < grid*grid;i++) {
+        let smallDiv = document.createElement("div");
+        smallDiv.classList.add("smallDiv");
+        smallDiv.dataset.smallDiv = i;
+        let flexbasis = 100/(grid);
+        container.style.flexBasis = flexbasis;
+        smallDiv.style.height = `${100/grid}%`;
+        smallDiv.style.width = `${100/grid}%`;
+        container.appendChild(smallDiv);
+        }
+};
+makeSmallDivs();
+let smallDivs = document.querySelectorAll(".smallDiv");
+
+function removeDivs() {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    };
+};
+
+gridButton.addEventListener("click", function () {
+    newGrid = prompt("Squares per side (Max 100)");
+    grid = newGrid;
+    removeDivs();
+    makeSmallDivs();
+
+});
+smallDivs = document.querySelectorAll(".smallDiv");
 function hover() {
     body.addEventListener("mouseover", function (e) {
         let mainId = e.target.parentElement.dataset.mainDiv;
         let childId = e.target.dataset.smallDiv;
         console.log(mainId);
         console.log(childId);
-        let child = document.querySelector(`[data-main-div ="${mainId}"] > [data-small-div= "${childId}"]`);
-        console.log(child);
-        child.style.cssText = "background-color: black";
-})}
+        let child = document.querySelector(`[data-small-div= "${childId}"]`);
+        child.style.backgroundColor = "black";
+})};
 body.addEventListener("click",hover);
